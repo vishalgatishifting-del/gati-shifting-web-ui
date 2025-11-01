0ea27ecad52663d6b0f7ecc63ce5d698bca8bb40
Vishal Kumar <vishalgatishifting@gmail.com>
2025-11-01 14:56:09 +0530

    add 24x7 line

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -246,7 +246,7 @@ const Home: React.FC = () => {
                     ))}
                 </Slider>
             </Box>
-
+                    <h1 className="trustLine">India’s Trusted Packers & Movers – 24/7 Support</h1>
             <motion.div
                 ref={ref1}
                 initial="hidden"
