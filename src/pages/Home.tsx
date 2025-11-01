14fba1d70c00238e1e667621fec3bdb1e40d58cc
Vishal Kumar <vishalgatishifting@gmail.com>
2025-11-01 14:47:38 +0530

    redesign home form

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -328,14 +328,14 @@ const Home: React.FC = () => {
                             <input type="text" placeholder={from} name="pickup_location_c" onChange={handleChange} value={formData.pickup_location_c} required />
                             <input type="text" placeholder={to} name="drop_location_c" onChange={handleChange} value={formData.drop_location_c} required />
                             <input type="text" placeholder={type} name="service_detail_c" onChange={handleChange} value={formData.service_detail_c} required />
-                            <button className="form-submit-btn" type="submit" disabled={loading}>
-                                {loading ? <CircularProgress size="30px" /> : "Get Free Quote"}
-                            </button>
                             <ReCAPTCHA
                                 ref={recaptchaRef}
                                 sitekey="6LfaOf4rAAAAAGZBXvb01FTAtYQoh0UXm4ChBDHV"
                                 onChange={(value: string | null) => setCaptchaValue(value)}
                             />
+                            <button className="form-submit-btn" type="submit" disabled={loading}>
+                                {loading ? <CircularProgress size="30px" /> : "Get Free Quote"}
+                            </button>
                         </form>
                         <span className="success-msg">{success && <p>{success}</p>}</span>
                     </div>
