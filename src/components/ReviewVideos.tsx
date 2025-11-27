71ead24dc2bb84e009a32209a7b93b26a98c0292
Vishal Kumar <vishalgatishifting@gmail.com>
2025-11-27 21:16:50 +0530

    change review videos

--- src/components/ReviewVideos.tsx
+++ src/components/ReviewVideos.tsx
@@ -8,11 +8,11 @@ const ReviewVideo = ()=>{
                 <h4>Real experiences from real people. Watch how we made their move stress-free.</h4>
 
                 <div className="video">
-                    <iframe width="400" height="255" src="https://www.youtube.com/embed/MlgLNz2-wbo?si=CWB7OKc-LM9dVgoa" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+                    <iframe width="400" height="255" src="https://www.youtube.com/embed/-l-_tRAUn2w?si=5D1-ktq_OxaF59R9" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
 
-                    <iframe width="400" height="255" src="https://youtube.com/embed/OMZEgiWrRfk?si=ImcWBYp5ve7AU-eJ" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+                    <iframe width="400" height="255" src="https://www.youtube.com/embed/KafXJZeP0mE?si=D8wqrw-b7cvKgLSE" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
 
-                    <iframe width="400" height="255" src="https://youtube.com/embed/1OkHdHUVhyY?si=F_-9Rte1auEfRJKA" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+                    <iframe width="400" height="255" src="https://www.youtube.com/embed/crpkuDgv_oA?si=cp_UHi8nlsx_Z_Dq" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
                 </div>
             </section>
     )
