a9dc49660cbaa04b4346013a6d1d60c7c33f5637
Vishal Kumar <vishalgatishifting@gmail.com>
2026-01-07 19:42:02 +0530

    add slider in home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -99,6 +99,13 @@ import Counter from "../components/Counter";
 import stepsImg from "../assets/5-steps-banner.png";
 
 
+
+import petSlider from "../assets/HomePage/slide2.png";
+import bike from "../assets/HomePage/slide3.png";
+import house from "../assets/HomePage/slide4.png";
+import car from "../assets/HomePage/slide1.png";
+
+
 // import { IFaqItem } from "../models/App.model";
 // import { AppConstant } from "../constants/app.constant";
 
@@ -197,6 +204,21 @@ const Home: React.FC<props> = ({ successCondition }) => {
         "best packers and movers",
         "cheap packers and movers",
         "packers and movers in delhi"]
+
+    const [activeIndex, setActiveIndex] = useState<number | null>(null);
+
+    interface ImageItem {
+        img: string;
+        heading: string;
+        text: string;
+    }
+
+    const imagesForC: ImageItem[] = [
+        { img: car, heading: "Trusted Movers & Packers", text: "As a trusted movers and packers company, we ensure protective wrapping, proper anchoring, and smooth doorstep pickup and delivery through our dedicated team." },
+        { img: house, heading: "Expert Team", text: "Our movers and packers team works with precision, ensuring protective wrapping, proper anchoring, and smooth doorstep pickup and delivery." },
+        { img: bike, heading: "House Shifting", text: "Smooth and reliable house shifting with professional packing, loading, transport, and setup at your new home." },
+        { img: petSlider, heading: "Bike Transport", text: "Bike relocation with protective wrapping, proper anchoring, and doorstep pickup and delivery." },
+    ];
 
 
     return (
@@ -399,11 +421,11 @@ const Home: React.FC<props> = ({ successCondition }) => {
                     <h1>Moving Services by <span className="primary-color">Gati Shifting Packers</span></h1>
                     <p>
                         Gati Shifting Packers PVT LTD. are always ready to help make the moving and packing experience excellent, more efficient and easier for you and your family. Just sit back, chill out and know that you are in safe hands!
-                        </p>
+                    </p>
 
-                        <p>Relocating to a new place can be a tiring and difficult process. There are many things to arrange and pack, including heavy furniture, electrical appliances and fragile and valuable antique items.</p>
+                    <p>Relocating to a new place can be a tiring and difficult process. There are many things to arrange and pack, including heavy furniture, electrical appliances and fragile and valuable antique items.</p>
 
-                       <p> Gati Shifting Packers's goal is to make your relocating experience as trouble-free as possible. Let us manage all of the difficult work. Our job is to be sure that your shifting is complete on time, every time.
+                    <p> Gati Shifting Packers's goal is to make your relocating experience as trouble-free as possible. Let us manage all of the difficult work. Our job is to be sure that your shifting is complete on time, every time.
                     </p>
                 </div>
             </section>
@@ -448,6 +470,22 @@ const Home: React.FC<props> = ({ successCondition }) => {
                     </div>
                 </div>
             </section>
+
+
+            <div className="image-box">
+                {imagesForC.map((data, index) => (
+                    <div
+                        key={index}
+                        className={`image-item ${activeIndex === index ? "active" : ""}`}
+                        onMouseEnter={() => setActiveIndex(index)}
+                        onMouseLeave={() => setActiveIndex(null)}
+                    >
+                        <h2>{data.heading}</h2>
+                        <p>{data.text}</p>
+                        <img src={data.img} alt={`img-${index}`} />
+                    </div>
+                ))}
+            </div>
 
             <GetInTouch></GetInTouch>
             <OfficeLocation />
