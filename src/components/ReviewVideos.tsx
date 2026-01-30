5043351b04702d31eefd253a7637bdc41e26b789
Vishal Kumar <vishalgatishifting@gmail.com>
2026-01-30 19:28:12 +0530

    enhance youtube video rendering

--- src/components/ReviewVideos.tsx
+++ src/components/ReviewVideos.tsx
@@ -1,20 +1,43 @@
 import "./ReviewVideos.scss"
 
-const ReviewVideo = ()=>{
+const ReviewVideo = () => {
+    document.querySelectorAll(".yt-placeholder").forEach(el => {
+        const element = el as HTMLElement;
+
+        element.addEventListener("click", () => {
+            const id = element.dataset.id;
+            if (!id) return;
+
+            element.innerHTML = `
+      <iframe
+      width="400" height="255"
+        src="https://www.youtube.com/embed/${id}?autoplay=1"
+        allow="autoplay; encrypted-media"
+        allowfullscreen
+      ></iframe>
+    `;
+        });
+    });
+
+
     return (
-        
-            <section id="customer-review">
-                <h1>What Our Customers Say</h1>
-                <h4>Real experiences from real people. Watch how we made their move stress-free.</h4>
 
-                <div className="video">
-                    <iframe width="400" height="255" src="https://www.youtube.com/embed/-l-_tRAUn2w?si=5D1-ktq_OxaF59R9" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+        <section id="customer-review">
+            <h1>What Our Customers Say</h1>
+            <h4>Real experiences from real people. Watch how we made their move stress-free.</h4>
 
-                    <iframe width="400" height="255" src="https://www.youtube.com/embed/KafXJZeP0mE?si=D8wqrw-b7cvKgLSE" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
-
-                    <iframe width="400" height="255" src="https://www.youtube.com/embed/crpkuDgv_oA?si=cp_UHi8nlsx_Z_Dq" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+            <div className="video">
+                <div className="yt-placeholder" data-id="-l-_tRAUn2w">
+                    <img style={{borderRadius: "20px"}} width="400" height="255" src="https://img.youtube.com/vi/-l-_tRAUn2w/hqdefault.jpg" />
                 </div>
-            </section>
+                <div className="yt-placeholder" data-id="KafXJZeP0mE">
+                    <img style={{borderRadius: "20px"}} width="400" height="255" src="https://img.youtube.com/vi/KafXJZeP0mE/hqdefault.jpg" />
+                </div>
+                <div className="yt-placeholder" data-id="crpkuDgv_oA">
+                    <img style={{borderRadius: "20px"}} width="400" height="255" src="https://img.youtube.com/vi/crpkuDgv_oA/hqdefault.jpg" />
+                </div>
+            </div>
+        </section>
     )
 }
 
