107e0e52f8bde35e912477c1c160fdfa7c048739
Vishal Kumar <vishalgatishifting@gmail.com>
2026-02-05 23:22:19 +0530

    fix source website error

--- src/api/formAPI.ts
+++ src/api/formAPI.ts
@@ -7,12 +7,12 @@ interface FormData {
   From: string;
   To: string;
   Goods: string;
-  SourceWebsite: string; 
+  SourceWebsite?: string; 
 }
 
 export const submitForm = (data: FormData) => {
   return axiosInstance.post("/create-lead", {
     ...data,
-    SourceWebsite: "Gati Shifting Packers",
+    SourceWebsite: data.SourceWebsite || "Gati Shifting Packers",
   });
 };
