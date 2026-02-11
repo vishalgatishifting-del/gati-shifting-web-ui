2d2d48fac2dadee47f36df5a48111d1ca262f9f9
Vishal Kumar <vishalgatishifting@gmail.com>
2026-02-11 09:11:45 +0530

    comment unused code

--- src/components/ZohoForm.tsx
+++ src/components/ZohoForm.tsx
@@ -2,7 +2,7 @@ import React, { useState, type FormEvent, type ChangeEvent } from 'react';
 import './ZohoForm.css';
 import { submitForm } from '../api/formAPI';
 import CircularProgress from '@mui/material/CircularProgress';
-import axios from 'axios';
+// import axios from 'axios';
 
 type FormData = {
   Name: string;
