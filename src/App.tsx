f2e622bbc3161a45dc8839f8e43c61f1eda1b158
Vishal Kumar <vishalgatishifting@gmail.com>
2026-04-05 14:39:17 +0530

    remove circular progress bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,21 @@
-// import { useState } from 'react'
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
 import './App.css'
 import AppRoutes from './routes/AppRoute'
-import CircularProgress from '@mui/material/CircularProgress';
-// import ChaportWidget from "./components/ChatPort.tsx";
 
 function App() {
-  useEffect(() => {
-  document.getElementById("pre-hero")?.remove();
-}, []);
-
-  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    const hideLoader = () => setLoading(false);
-
-    // Agar document already load ho chuka hai
-    if (document.readyState === "complete") {
-      hideLoader();
-    } else {
-      window.addEventListener("load", hideLoader);
-      return () => window.removeEventListener("load", hideLoader);
+    const preHero = document.getElementById("pre-hero");
+    if (preHero) {
+      preHero.remove();
     }
   }, []);
 
-  if (loading) {
-    return (
-      <div className="preloader">
-        <CircularProgress />
-      </div>
-    );
-  }
-
   return (
     <>
-        <AppRoutes></AppRoutes>
-        {/* <ChaportWidget /> */}
+      <AppRoutes />
     </>
   )
 }
 
-export default App
+export default App;
